f498c02b18a3eda1ad6d58ad7ea618f4869a2949
Roman Flössler <flor@flor.cz>
2021-08-27 22:06:22 +0200

    just another update

--- wordpress/wp_files/ssg_cfg.js
+++ wordpress/wp_files/ssg_cfg.js
@@ -1,7 +1,7 @@
 
 SSG.cfg.wordpressGalleryFS = true;
 SSG.cfg.respectOtherWpGalleryPlugins = true;
-SSG.cfg.captionExif = 'trim';
+SSG.cfg.captionExif = window.screen.width > 933 ? 'trim' : 'icon';
 SSG.cfg.globalAuthorCaption =  "photo by Flor" ;
 SSG.cfg.preferedCaptionLocation =  8;
 
