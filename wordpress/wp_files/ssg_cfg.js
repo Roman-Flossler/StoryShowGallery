de1d85799512d5cb7720633215526b05532d2ba1
Roman Flössler <flor@flor.cz>
2021-08-29 14:58:53 +0200

    v2.11.2 - better EXIF formatting, bugfix

--- wordpress/wp_files/ssg_cfg.js
+++ wordpress/wp_files/ssg_cfg.js
@@ -9,11 +9,12 @@ SSG.cfg.preferedCaptionLocation =  8;
 // EXAMPLE: SSG.cfg.watermarkText = '© Mia Photo'; OR  SSG.cfg.watermarkImage = 'https://www.flor.cz/img/florcz.png';
 SSG.cfg.watermarkWidth = 147; // watermark width in pixels, it is downsized on smaller screens.
 SSG.cfg.watermarkImage = '';  // watermark image URL
-SSG.cfg.watermarkText = 'ꐠ SSG';  //  watermark text, text will wrap according to watermark width
 SSG.cfg.watermarkOffsetX = 2; // watermark horizontal offset from left in percents
 SSG.cfg.watermarkOffsetY = 2; // watermark vertical offset from bottom in percent of display size
-SSG.cfg.watermarkOpacity = 0.36; // opacity
-SSG.cfg.watermarkFontSize = 20; // font size in pixels  
+SSG.cfg.watermarkOpacity = 0.42; // opacity
+
+SSG.cfg.watermarkText = window.screen.width > 933 ? '<b>ꐠ</b> Story Show Gallery' : '<b>ꐠ</b> SSG';
+SSG.cfg.watermarkFontSize = window.screen.width > 933 ? 18 : 20;
 
 // URL of the HTML file to load behind the gallery (usually a signpost to other galleries). Set to null if you don't want it.
 SSG.cfg.fileToLoad = 'https://roman-flossler.github.io/StoryShowGallery/play.html';   // More info about signpost is at https://ssg.flor.cz/
